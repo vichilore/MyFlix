345211062953d6a87ff46323ea1e65ef7560594e
Lorenzo <vichilore>
2025-10-27 19:31:22 +0100

    try deploy render

--- api-server/server.js
+++ api-server/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import pkg from "pg";
 
 dotenv.config();
